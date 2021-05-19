b5de6afdd90ec19c9b76e6e8d782a6723e8fcdef
ridddyndoma <riddyndoma@gmail.com>
2021-05-19 22:30:29 +0200

    update prep functions

--- preparation/prep_functions.R
+++ preparation/prep_functions.R
@@ -5,36 +5,47 @@ library(dplyr)
 prepareDFWinnersFollowedByMonth <- function(jul20,aug20,sep20,oct20,nov20,dec20,jan21,fev21,mar21,avr21,may21) {
   
   jul20 <- jul20 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   aug20 <- aug20 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   sep20 <- sep20 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   oct20 <- oct20 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   nov20 <- nov20 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   dec20 <- dec20 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   jan21 <- jan21 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   fev21 <- fev21 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   mar21 <- mar21 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   avr21 <- avr21 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   may21 <- may21 %>%
+    filter(statusId !='LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED') %>%
     select(personId,date,statusId,contact.gender,contact.visualId)
   
   answer <- rbind(jul20,aug20,sep20,oct20,nov20,dec20,jan21,fev21,mar21,avr21,may21)
